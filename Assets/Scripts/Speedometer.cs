9a0d86a811588895ada7c3b7f5c3c961d0a02f7e
SakDrakken <adamkrasa@seznam.cz>
2019-01-08 18:03:59 +0100

    added UI and speedometer

---
+++ Assets/Scripts/Speedometer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Speedometer : MonoBehaviour {
+
+    public Rigidbody player;
+    Text content;
+	
+    void Start()
+    {
+        content = GetComponent<Text>();
+    }
+
+	void FixedUpdate () {
+        content.text = Convert.ToString(Convert.ToInt32(player.velocity.magnitude));
+	}
+}
